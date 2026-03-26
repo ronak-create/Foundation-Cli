f64e7172f3ea9cf0d3d6fa90f53f776a2cdb32c9
ronak parmar <84374383+ronak-create@users.noreply.github.com>
2026-03-26 19:42:31 +0530

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://ronak-create.github.io/Foundation-Cli",
